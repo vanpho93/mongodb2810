497b2038af387bf38886ae08845583c2a6ca488f
Van Pho Nguyen <vanpho02@gmail.com>
2017-12-02 18:45:17 +0700

    objectid

--- index.js
+++ index.js
@@ -5,3 +5,23 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/mean2810', { useMongoClient: true })
 .then(() => console.log('Connected'))
 .catch(err => console.log(err.message));
+
+const productSchema = new mongoose.Schema({
+    name: { type: String, require: true, unique: true, trim: true },
+    price: { type: Number },
+    description: { type: String, require: true },
+    image: { type: String }
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+const laptop = new Product({
+    name: 'Surface 2017',
+    price: 1000,
+    description: 'abcd',
+    image: '1.jpg'
+});
+
+// laptop.save()
+// .then(x => console.log(x))
+// .catch(x => console.log(x))
