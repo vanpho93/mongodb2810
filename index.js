592168ccc6092559e3e46ec1b1efba2e80b00975
Van Pho Nguyen <vanpho02@gmail.com>
2017-12-02 19:05:52 +0700

    xong

--- index.js
+++ index.js
@@ -15,13 +15,53 @@ const productSchema = new mongoose.Schema({
 
 const Product = mongoose.model('Product', productSchema);
 
-const laptop = new Product({
-    name: 'Surface 2017',
-    price: 1000,
-    description: 'abcd',
-    image: '1.jpg'
-});
+// Create new doc
 
-// laptop.save()
-// .then(x => console.log(x))
+    // const laptop = new Product({
+    //     name: 'Acer 2017',
+    //     price: 2000,
+    //     description: 'abcd',
+    //     image: '1.jpg'
+    // });
+
+    // laptop.save()
+    // .then(x => console.log(x))
 // .catch(x => console.log(x))
+
+// SELECT - Find docs
+
+    // Product.find({})
+    // .then(products => console.log(products));
+
+    // Product.find({ price: 2000 })
+    // .then(products => console.log(products));
+
+    // Product.find({ price: { $gte: 1000 } })
+    // .then(products => console.log(products));
+
+    // Product.findById('5a22928c8b76b9381f8b0398')
+    // .then(product => console.log(product));
+
+    // Product.findOne({ name: 'Mac 2017' })
+// .then(product => console.log(product));
+
+// UPDATE docs
+    // Product.findByIdAndUpdate('5a22928c8b76b9381f8b0398', { price: 150 })
+    // .then(product => console.log(product));
+
+    // Product.findOneAndUpdate({ name: 'Mac 2017' }, { price: 200 })
+    // .then(product => console.log(product));
+
+    // Product.update({ name: 'Mac 2017' }, { price: 200 })
+// .then(product => console.log(product));
+
+// REMOVE docs
+
+// Product.remove({ price: 0 })
+// .then(result => console.log(result));
+
+// Product.findOneAndRemove({ name: 'Lenovo 2017' })
+// .then(product => console.log(product));
+
+Product.findByIdAndRemove('5a229294857fc2382df89e48')
+.then(product => console.log(product));
